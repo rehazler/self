54c8bd6a0f850a0b21107633ac226c6b7d998c15
rehazler <rehazler@gmail.com>
2018-12-30 17:04:46 -0800

    Backup changes before css change to sass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'self';
+  
+	burgerMenuToggle: boolean = false;
+
+	toggleBurgerMenu(){
+		return this.burgerMenuToggle = !this.burgerMenuToggle;
+	}
+
+
+
 }
